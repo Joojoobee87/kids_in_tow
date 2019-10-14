7c9219cc72fad7dbafd13533bc2ec2ada5743c83
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-10-14 15:30:34 +0000

    removed variable and script tags from index.html and add an event listener for when markers are clicked on

--- js/script.js
+++ js/script.js
@@ -30,11 +30,13 @@ function addMarkerToMap(map) {
 
         var place = placesOfInterest[i];
         var marker = new google.maps.Marker({
-            position:{lat: place[1], lng: place[2] },
+            position: { lat: place[1], lng: place[2] },
             map: map,
             title: place[0],
         });
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        })
     }
 }
 
-//_.wd=function(a){for(var b=[],c=0;c<arguments.length;++c)b[c-0]=arguments[c];_.y.console&&_.y.console.error&&_.y.console.error.apply(_.y.console,_.Da(b))};xd=function(a){this.message=a;this.name="InvalidValueError";this.stack=Error().stack};_.yd=function(a,b){var c="";if(null!=b){if(!(b instanceof xd))return b;c=": "+b.message}return new xd(a+c)};_.zd=function(a){if(!(a instanceof xd))throw a;_.wd(a.name+": "+a.message)};
