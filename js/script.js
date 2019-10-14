ed69c77d183510b2c155970aa3b5fbc1c32fa76e
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-10-14 15:15:23 +0000

    moved js to the script.js file. Added new variable for placesOfInterest and created a function to add markers for various places of interest

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
-function initMap() {
-    let map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 53.8008, lng: -1.5491},
-          zoom: 8
-        });
-      }
+// Initialise Google Map with marker at Leeds City Centre
 
-let marker = new google.maps.Marker({
-    position: tbc,
-    map: map
-})
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 53.802156, lng: -1.548946 },
+        zoom: 10
+    });
+
+    let markerLeedsCentre = new google.maps.Marker({
+        position: { lat: 53.802156, lng: -1.548946 },
+        map: map
+    });
+
+    addMarkerToMap(map);
+}
+
+//define places of interest including name and coordinates
+
+let placesOfInterest = [
+    ["Harewood House", 53.8999, -1.5115, false],
+    ["Stockeld Park", 53.940942, -1.431498],
+    ["Valley Gardens", 53.9933, -1.5478, false],
+]
+
+//function to add markers to map, iterating through the placesOfInterest variable
+
+function addMarkerToMap(map) {
+
+    for (var i = 0; i < placesOfInterest.length; i++) {
+
+        var place = placesOfInterest[i];
+        var marker = new google.maps.Marker({
+            position:{lat: place[1], lng: place[2] },
+            map: map,
+            title: place[0],
+        });
+    }
+}
+
+//_.wd=function(a){for(var b=[],c=0;c<arguments.length;++c)b[c-0]=arguments[c];_.y.console&&_.y.console.error&&_.y.console.error.apply(_.y.console,_.Da(b))};xd=function(a){this.message=a;this.name="InvalidValueError";this.stack=Error().stack};_.yd=function(a,b){var c="";if(null!=b){if(!(b instanceof xd))return b;c=": "+b.message}return new xd(a+c)};_.zd=function(a){if(!(a instanceof xd))throw a;_.wd(a.name+": "+a.message)};
