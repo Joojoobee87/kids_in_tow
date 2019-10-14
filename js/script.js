bda2ba469af5d27fb9042831fd904f4b00b2e5b0
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-10-14 20:43:33 +0000

    added info windows to Google Map marker

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Initialise Google Map with marker at Leeds City Centre
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+
+    let map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 53.802156, lng: -1.548946 },
         zoom: 10
     });
@@ -17,9 +18,9 @@ function initMap() {
 //define places of interest including name and coordinates
 
 let placesOfInterest = [
-    ["Harewood House", 53.8999, -1.5115, false],
-    ["Stockeld Park", 53.940942, -1.431498],
-    ["Valley Gardens", 53.9933, -1.5478, false],
+    ["Harewood House", 53.8999, -1.5115, false, "This is the content of Harewood House"],
+    ["Stockeld Park", 53.940942, -1.431498, false, "This is the content of Stockeld Park"],
+    ["Valley Gardens", 53.9933, -1.5478, false, "This is the content of Valley Gardens"],
 ]
 
 //function to add markers to map, iterating through the placesOfInterest variable
@@ -32,11 +33,15 @@ function addMarkerToMap(map) {
         var marker = new google.maps.Marker({
             position: { lat: place[1], lng: place[2] },
             map: map,
-            title: place[0],
+            title: place[0]
         });
+
+        let infoWindow = new google.maps.InfoWindow({
+            content: place[4]
+        });
+
         marker.addListener('click', function() {
-            infowindow.open(map, marker);
-        })
+            infoWindow.open(map, marker);
+        });
     }
 }
-
