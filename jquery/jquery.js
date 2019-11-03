1d12d868b2d383aaaf6757ade3a9148872473c5f
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-11-03 16:36:08 +0000

    applied a bootstrap shown.bs.modal function to pick up only the image slides within the current open modal and not loop round all images in the .modal-image class

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -22,44 +22,6 @@ $(document).ready(function() {
     }
     filterSelected();
 
-    function slideshow() {
-
-        let images = Array.from($(".modal-image > img"));
-        let nextButton = $(".next");
-        let prevButton = $(".prev");
-        let index = 1;
-
-        $(images[0]).css("display", "block");
-
-        $(nextButton).on('click', function() {
-            for (i = 0; i < images.length; i++) {
-                $(images[i]).css("display", "none");
-            }
-            index++;
-            console.log(index)
-            if (index > images.length - 1) {
-                $(images[images.length - 1]).css("display", "block");
-                index = 0;
-                return false;
-            }
-            $(images[index - 1]).css("display", "block");
-        })
-
-        $(prevButton).on('click', function() {
-            for (i = 0; i < images.length; i++) {
-                $(images[i]).css("display", "none");
-            }
-            index--;
-            console.log(index);
-            if (index < 1) {
-                $(images[images.length + 1]).css("display", "block");
-                index = images.length
-            }
-            $(images[index - 1]).css("display", "block");
-        })
-    }
-    slideshow();
-
     function dropdownContainers() {
         $(".filters-tab").click(function() {
             $(".tab-container").css("display", "none");
@@ -75,4 +37,48 @@ $(document).ready(function() {
         })
     }
     dropdownContainers();
+
+    function modalSlideshow() {
+        $('.modal').on('shown.bs.modal', function(event) {
+            var modal = $(this)
+            console.log(modal);
+            let images = Array.from(modal.find('.modal-image').children("img"));
+            console.log(images);
+            let nextButton = $(".next");
+            let prevButton = $(".prev");
+            let index = 1;
+
+            $(images).css("display", "none");
+            $(images[0]).css("display", "block");
+            console.log(images[0]);
+
+            $(nextButton).on('click', function() {
+                for (i = 0; i < images.length; i++) {
+                    $(images[i]).css("display", "none");
+                }
+                index++;
+                console.log(index)
+                if (index > images.length - 1) {
+                    $(images[images.length - 1]).css("display", "block");
+                    index = 0;
+                    return false;
+                }
+                $(images[index - 1]).css("display", "block");
+            })
+
+            $(prevButton).on('click', function() {
+                for (i = 0; i < images.length; i++) {
+                    $(images[i]).css("display", "none");
+                }
+                index--;
+                console.log(index);
+                if (index < 1) {
+                    $(images[images.length + 1]).css("display", "block");
+                    index = images.length
+                }
+                $(images[index - 1]).css("display", "block");
+            })
+        })
+    }
+    modalSlideshow();
 })
