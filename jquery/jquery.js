a4cf78e99bc08eb0479edb38dfca7d2b048d5f09
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-11-15 11:57:27 +0000

    updated applyFilters function for displaying activities

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     function dropdownContainers() {
-        $(".filters-tab").click(function() {
+        $(".filters-tab").click(function () {
             $(".tab-container").css("display", "none");
             $(".filters-container").slideToggle();
         })
-        $(".about-tab").click(function() {
+        $(".about-tab").click(function () {
             $(".tab-container").css("display", "none");
             $(".about-container").slideToggle();
         })
-        $(".contact-tab").click(function() {
+        $(".contact-tab").click(function () {
             $(".tab-container").css("display", "none");
             $(".contact-container").slideToggle();
         })
@@ -17,7 +17,7 @@ $(document).ready(function() {
     dropdownContainers();
 
     function modalSlideshow() {
-        $('.modal').on('shown.bs.modal', function(event) {
+        $('.modal').on('shown.bs.modal', function (event) {
             var modal = $(this)
             console.log(modal);
             let images = Array.from(modal.find('.modal-image').children("img"));
@@ -28,7 +28,7 @@ $(document).ready(function() {
             let prevButton = $(".prev");
             let index = 1;
 
-            $(nextButton).on('click', function() {
+            $(nextButton).on('click', function () {
                 for (i = 0; i < images.length; i++) {
                     $(images[i]).css("display", "none");
                 }
@@ -42,7 +42,7 @@ $(document).ready(function() {
                 $(images[index - 1]).css("display", "block");
             })
 
-            $(prevButton).on('click', function() {
+            $(prevButton).on('click', function () {
                 for (i = 0; i < images.length; i++) {
                     $(images[i]).css("display", "none");
                 }
@@ -58,35 +58,35 @@ $(document).ready(function() {
     }
     modalSlideshow();
 
-
     function applyFilters() {
 
         let button = $(".activity-btn"); // filter buttons
-        console.log(button);
-        let applyButton = $(".apply-filters"); // apply all filters button
-        console.log(applyButton);
 
-        button.on('click', function() {
-            $(this).toggleClass('highlight'); //switch highlight class on and off clicked button
-            console.log(this);
-            let selected = $(".highlight") // all buttons highlighted, get <i> class
-            console.log(selected);
-            let buttonId = $(selected).attr("id");
-            console.log(buttonId);
-            let filters = []
+        button.on('click', function () {
+            $(this).toggleClass('highlight') //switch highlight class on and off clicked button
+            let selected = $(".highlight") // all buttons highlighted
+            let filters = [] //initialise filters variable
 
-            $('.highlight').each(function() {
-                let highlightedIds = $(this).attr('id');
+            $(selected).each(function () {
+                let highlightedIds = $(this).attr('id'); //gets id of highlighted button
                 filters.push(highlightedIds);
-                console.log(filters);
 
-                for (var i = 0; i < filters.length; i++) {
-                    console.log(filters[i]);
-                    if ($(".activity-section").hasClass(filters[i])) {
-                        console.log(filters[i]);
-                        $(".activity-section").css("display", "none");
-                    }
-                }
+                let applyButton = $(".apply-filters"); // apply filters button
+
+                $(applyButton).on('click', function () { // on click of apply filters button
+                    let activity = $(".activity-section")
+                    $(activity).hide();                     // hide all activity sections
+
+                    $.each(filters, function (index, value) { // for each of the filters selected
+                        if (activity.className == value) { // if the activity class is == value then show else hide
+                            $(activity).show();
+                        } else {
+                            $(activity).hide();
+                        }
+                    });
+
+                });
+
             });
         });
     }
