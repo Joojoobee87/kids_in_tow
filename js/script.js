c490e0a1a18f3f364a9b6ef8260069f0bae56a56
Joojoobee87 <joanneingle1987@yahoo.co.uk>
2019-11-18 12:34:03 +0000

    updated form type and names

--- js/script.js
+++ js/script.js
@@ -114,20 +114,3 @@ function addMarkerToMap(map) {
         });
     }
 }
-
-function sendEmail(contactForm) {
-    emailjs.send("gmail", "kids_in_tow_contact", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value,
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}
